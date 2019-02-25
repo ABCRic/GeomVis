9a8269f6b49aaae87cc8d7ebded49a2a038a369d
ABCRic <ABCRic@users.noreply.github.com>
2019-02-25 19:18:44 +0000

    Add undo/redo code for lines

--- src/geomvis.ts
+++ src/geomvis.ts
@@ -10,6 +10,31 @@ import * as SVG from "svg.js";
 import "svg.select.js";
 import "svg.resize.js";
 import "svg.draggable.js";
+import "svg.draw.js";
+
+abstract class Action {
+    abstract undo() : void;
+    abstract redo() : void;
+}
+
+let undoStack : Action[] = [];
+let redoStack : Action[] = [];
+
+export function undo() {
+    let act = undoStack.pop();
+    console.log(act);
+    if(!act) return;
+    act.undo();
+    redoStack.push(act);
+}
+
+export function redo() {
+    let act = redoStack.pop();
+    console.log(act);
+    if(!act) return;
+    act.redo();
+    undoStack.push(act);
+}
 
 let theSVG : SVG.Doc;
 let d3SVG : d3.Selection<d3.BaseType, {}, HTMLElement, any>;
@@ -38,22 +63,51 @@ export function onLoad() {
         .attr("cx", (d: any) => d.x)
         .attr("cy", (d: any) => d.y);*/
 
-    // add box for capturing canvas clicks
-    let clickBox = d3SVG.append("rect")
-        .attr("x", 0)
-        .attr("y", 0)
-        .attr("width", "100%")
-        .attr("height", "100%")
-        .attr("fill", "white")
-        .attr("fill-opacity", 0)
-        // with line drawing for cohen-sutherland
-        .call(lineDraw);
+    // // add box for capturing canvas clicks
+    // let clickBox = d3SVG.append("rect")
+    //     .attr("x", 0)
+    //     .attr("y", 0)
+    //     .attr("width", "100%")
+    //     .attr("height", "100%")
+    //     .attr("fill", "white")
+    //     .attr("fill-opacity", 0)
+    //     // with line drawing for cohen-sutherland
+    //     .call(lineDraw);
 
     // add example resizable rect
     var rect = theSVG.rect(200,100).move(100,100).fill('#fff')
     rect.selectize({rotationPoint: false});
     rect.resize();
     rect.draggable();
+
+    // setup line drawing
+    {
+        let line : SVG.Line;
+
+        theSVG.on('mousedown', function(event) {
+            // start drawing
+            line = theSVG.line(0,0,0,0).stroke({color: "black", width: 1}).draw(event, {});
+        });
+        theSVG.on('mouseup', function(event){
+            // finish drawing
+            line.draw("stop", event);
+
+            // add to history
+            undoStack.push(new class extends Action {
+                path: SVG.Line;
+                constructor(path: SVG.Line) {
+                    super();
+                    this.path = path;
+                }
+                undo() {
+                    line.remove();
+                }
+                redo() {
+                    theSVG.add(line);
+                }
+            }(line));
+        });
+    }
 
     // setup pseudo-code panel
     let pseudoCodePanel = document.getElementById("pseudocodepanel")!;
@@ -63,35 +117,4 @@ export function onLoad() {
         p.classList.add("mb-0"); // remove bottom margin
         pseudoCodePanel.appendChild(p);
     });
-}
-
-function lineDraw(selection: any) {
-    var xy0 : any, 
-        path : any, 
-        keep = false, 
-        line = d3.line()
-                 .x(d => d[0])
-                 .y(d => d[1]);
-
-    selection
-        .on('mousedown', function(this: any){ 
-            keep = true;
-            xy0 = d3.mouse(this);
-            path = d3SVG.append('path')
-                     .attr('d', line([xy0, xy0]) as string)
-                     .attr("stroke", "black")
-                     .attr("stroke-width", "1px");
-            console.log(path);
-        })
-        .on('mouseup', function(){ 
-            keep = false;
-        })
-        .on('mousemove', function(this: any){ 
-            if (keep) {
-                let Line = line([xy0, d3.mouse(this).map(function(x){ return x - 1; })]);
-                console.log(Line);
-                console.log(path);
-                path.attr('d', Line);
-            }
-        });
 }
