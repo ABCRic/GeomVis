a06dff49d102431dbcf9572f2a260bf9f6de00ba
ABCRic <ABCRic@users.noreply.github.com>
2019-03-06 01:52:57 +0000

    Fix a ton of lint issues

--- src/geomvis.ts
+++ src/geomvis.ts
@@ -1,33 +1,30 @@
-import "svg.select.js/dist/svg.select.css";
-
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import * as d3 from "d3";
-import { pseudoCode } from "./cohensutherland";
-import * as SVG from "svg.js";
-
-import "svg.select.js";
-import "svg.resize.js";
 import "svg.draggable.js";
 import "svg.draw.js";
+import * as SVG from "svg.js";
+import "svg.resize.js";
+import "svg.select.js";
+import "svg.select.js/dist/svg.select.css";
+import { pseudoCode } from "./cohensutherland";
 import "./deps/svg.topoly.js";
 import { svgLineLength } from "./utils";
 
 const LEFT_MOUSE_BUTTON = 0;
 
 abstract class Action {
-    abstract undo() : void;
-    abstract redo() : void;
+    public abstract undo(): void;
+    public abstract redo(): void;
 }
 
-let undoStack : Action[] = [];
-let redoStack : Action[] = [];
+const undoStack: Action[] = [];
+const redoStack: Action[] = [];
 
 export function undo() {
-    let act = undoStack.pop();
+    const act = undoStack.pop();
     console.log(act);
-    if(!act) return;
+    if (!act) return;
     act.undo();
     redoStack.push(act);
 
@@ -35,9 +32,9 @@ export function undo() {
 }
 
 export function redo() {
-    let act = redoStack.pop();
+    const act = redoStack.pop();
     console.log(act);
-    if(!act) return;
+    if (!act) return;
     act.redo();
     undoStack.push(act);
 
@@ -45,10 +42,10 @@ export function redo() {
 }
 
 function updateUndoRedoButtons() {
-    let undoButton = document.getElementById("undobutton") as HTMLButtonElement;
-    undoButton!.disabled = undoStack.length == 0;
-    let redoButton = document.getElementById("redobutton") as HTMLButtonElement;
-    redoButton!.disabled = redoStack.length == 0;
+    const undoButton = document.getElementById("undobutton") as HTMLButtonElement;
+    undoButton!.disabled = undoStack.length === 0;
+    const redoButton = document.getElementById("redobutton") as HTMLButtonElement;
+    redoButton!.disabled = redoStack.length === 0;
 }
 
 function pushToUndoHistory(act: Action) {
@@ -59,52 +56,52 @@ function pushToUndoHistory(act: Action) {
 
 let shiftDown = false;
 function setShiftDown(event: KeyboardEvent) {
-    if(event.keyCode === 16 || event.charCode === 16){
+    if (event.keyCode === 16 || event.charCode === 16) {
         shiftDown = true;
     }
 }
 
 function setShiftUp(event: KeyboardEvent) {
-    if(event.keyCode === 16 || event.charCode === 16){
+    if (event.keyCode === 16 || event.charCode === 16) {
         shiftDown = false;
     }
 }
 
-let theSVG : SVG.Doc;
-let d3SVG : d3.Selection<d3.BaseType, {}, HTMLElement, any>;
+let theSVG: SVG.Doc;
+let d3SVG: d3.Selection<d3.BaseType, {}, HTMLElement, any>;
 
 let currentStep = 0;
 
 export function forward() {
-    let oldStep = currentStep;
-    if(currentStep < pseudoCode.length - 1)
+    const oldStep = currentStep;
+    if (currentStep < pseudoCode.length - 1)
         currentStep++;
     updatePseudoCodeHighlight(oldStep);
 }
 
 export function back() {
-    let oldStep = currentStep;
-    if(currentStep > 0)
+    const oldStep = currentStep;
+    if (currentStep > 0)
         currentStep--;
     updatePseudoCodeHighlight(oldStep);
 }
 
 export function playPause() {
-    if(isPlaying) pause();
+    if (isPlaying) pause();
     else play();
 }
 
 let isPlaying = false;
 function play() {
     isPlaying = true;
-    var id = setInterval(iter, 800)
+    const id = setInterval(iter, 800);
     function iter() {
-        if(isPlaying) {
+        if (isPlaying) {
             forward();
         } else {
             clearInterval(id);
         }
-    };
+    }
 }
 
 function pause() {
@@ -112,63 +109,63 @@ function pause() {
 }
 
 function updatePseudoCodeHighlight(oldStep: number) {
-    let items = (document.getElementById("pseudocodepanel") as HTMLDivElement).childNodes as NodeListOf<HTMLPreElement>;
+    const items = (document.getElementById("pseudocodepanel") as HTMLDivElement).childNodes as NodeListOf<HTMLPreElement>;
     items[oldStep].classList.remove("pseudocode-currentline");
     items[currentStep].classList.add("pseudocode-currentline");
-    let stepTextPanel = document.getElementById("steptextpanel") as HTMLDivElement;
+    const stepTextPanel = document.getElementById("steptextpanel") as HTMLDivElement;
     stepTextPanel.innerText = pseudoCode[currentStep].stepText;
 }
 
 export function fileSelected() {
-    let file = ((document.getElementById("fileuploader") as HTMLInputElement).files||[null])[0];
-    if(file) {
-        let reader = new FileReader();
+    const file = ((document.getElementById("fileuploader") as HTMLInputElement).files || [null])[0];
+    if (file) {
+        const reader = new FileReader();
         reader.readAsText(file); // result will be string in reader.result
-        reader.onload = function(event) {
-            let fileContents = reader.result as string;
+        reader.onload = event => {
+            const fileContents = reader.result as string;
             loadFile(fileContents);
-        }
-        reader.onerror = function(event) {
+        };
+        reader.onerror = event => {
             // todo: better error
             (document.getElementById("pseudocodepanel") as HTMLDivElement).innerText = "Error reading file.";
-        }
+        };
     }
 }
 
-let rect : SVG.Rect;
-let lines : SVG.Line[];
+let rect: SVG.Rect;
+let lines: SVG.Line[];
 
 function loadFile(contents: string) {
-    let doc = new DOMParser().parseFromString(contents, "image/svg+xml");
-    let docRect = doc.getElementsByTagName("rect")[0];
-    let docLines = doc.getElementsByTagName("line");
-    let docPaths = doc.getElementsByTagName("path");
+    const doc = new DOMParser().parseFromString(contents, "image/svg+xml");
+    const docRect = doc.getElementsByTagName("rect")[0];
+    const docLines = doc.getElementsByTagName("line");
+    const docPaths = doc.getElementsByTagName("path");
 
     // todo: clear current document
-    
+
     // adapt our rect to first rect in document
-    let adoptedRect = SVG.adopt(docRect);
+    const adoptedRect = SVG.adopt(docRect);
     rect.attr("x", adoptedRect.x());
     rect.attr("y", adoptedRect.y());
     rect.attr("width", adoptedRect.width());
     rect.attr("height", adoptedRect.height());
 
     // fetch lines from document
-    for(const line of docLines) {
-        let adoptedLine = SVG.adopt(line) as SVG.Line;
+    for (const line of docLines) {
+        const adoptedLine = SVG.adopt(line) as SVG.Line;
         theSVG.add(adoptedLine);
         lines.push(adoptedLine);
     }
 
     // fetch paths from document and convert them into lines
-    for(const path of docPaths) {
-        let adoptedPath = SVG.adopt(path) as SVG.Path;
-        let polyline = adoptedPath.toPoly() as SVG.PolyLine;
-        
-        let points = polyline.array().value as unknown as number[][];
-        for(let i = 0; i < points.length - 1; i++) {
-            let x = new SVG.Array();
-            let line = theSVG.line(
+    for (const path of docPaths) {
+        const adoptedPath = SVG.adopt(path) as SVG.Path;
+        const polyline = adoptedPath.toPoly() as SVG.PolyLine;
+
+        const points = polyline.array().value as unknown as number[][];
+        for (let i = 0; i < points.length - 1; i++) {
+            const x = new SVG.Array();
+            const line = theSVG.line(
                 points[i][0],
                 points[i][1],
                 points[i + 1][0],
@@ -219,7 +216,7 @@ export function onLoad() {
     //     .call(lineDraw);
 
     // add example resizable rect
-    rect = theSVG.rect(200,100).move(100,100).fill('#fff')
+    rect = theSVG.rect(200, 100).move(100, 100).fill("#fff");
     rect.selectize({rotationPoint: false});
     rect.resize();
     rect.draggable();
@@ -235,24 +232,24 @@ export function onLoad() {
             height: number;
         }
         class RectModifyAction extends Action {
-            oldData: RectData;
-            newData: RectData;
-            target: SVGRectElement;
+            private oldData: RectData;
+            private newData: RectData;
+            private target: SVGRectElement;
             constructor(oldData: RectData, newData: RectData, target: SVGRectElement) {
                 super();
                 this.oldData = oldData;
                 this.newData = newData;
                 this.target = target;
             }
-            undo() {
-                let adopted = SVG.adopt(this.target);
+            public undo() {
+                const adopted = SVG.adopt(this.target);
                 adopted.attr("x", this.oldData.x);
                 adopted.attr("y", this.oldData.y);
                 adopted.attr("width", this.oldData.width);
                 adopted.attr("height", this.oldData.height);
             }
-            redo() {
-                let adopted = SVG.adopt(this.target);
+            public redo() {
+                const adopted = SVG.adopt(this.target);
                 adopted.attr("x", this.newData.x);
                 adopted.attr("y", this.newData.y);
                 adopted.attr("width", this.newData.width);
@@ -260,10 +257,10 @@ export function onLoad() {
             }
         }
 
-        let oldData : RectData;
-        let newData : RectData;
+        let oldData: RectData;
+        let newData: RectData;
         rect.on("resizestart", (event: any) => {
-            let adopted = SVG.adopt(event.target);
+            const adopted = SVG.adopt(event.target);
             oldData = {
                 x: adopted.x(),
                 y: adopted.y(),
@@ -272,7 +269,7 @@ export function onLoad() {
             };
         });
         rect.on("resizing", (event: any) => {
-            let adopted = SVG.adopt(event.target);
+            const adopted = SVG.adopt(event.target);
             newData = {
                 x: adopted.x(),
                 y: adopted.y(),
@@ -286,12 +283,12 @@ export function onLoad() {
 
         rect.on("beforedrag", function(this: SVG.Rect, event: any) {
             // drag only if shift is pressed
-            if(!shiftDown) {
+            if (!shiftDown) {
                 event.preventDefault();
                 return;
             }
 
-            let adopted = SVG.adopt(event.target);
+            const adopted = SVG.adopt(event.target);
             oldData = {
                 x: adopted.x(),
                 y: adopted.y(),
@@ -303,7 +300,7 @@ export function onLoad() {
                 y: this.y(),
                 width: this.width(),
                 height: this.height(),
-            }
+            };
         });
 
         rect.on("dragend", function(this: SVG.Rect, event: any) {
@@ -312,31 +309,31 @@ export function onLoad() {
                 y: this.y(),
                 width: this.width(),
                 height: this.height(),
-            }
+            };
             pushToUndoHistory(new RectModifyAction(oldData, newData, event.target));
         });
     }
 
     // setup line drawing
     {
-        let line : SVG.Line | null;
+        let line: SVG.Line | null;
 
-        theSVG.on('mousedown', function(event:MouseEvent) {
-            if(event.button != LEFT_MOUSE_BUTTON)
+        theSVG.on("mousedown", function(event: MouseEvent) {
+            if (event.button !== LEFT_MOUSE_BUTTON)
                 return;
 
             // start drawing
-            line = theSVG.line(0,0,0,0).stroke({color: "black", width: 1}).draw(event, {});
+            line = theSVG.line(0, 0, 0, 0).stroke({color: "black", width: 1}).draw(event, {});
         });
-        theSVG.on('mouseup', function(event:MouseEvent){
-            if(!line)
+        theSVG.on("mouseup", function(event: MouseEvent) {
+            if (!line)
                 return;
-            
+
             // finish drawing
             line.draw("stop", event);
 
             // ignore lines that are too small
-            if(svgLineLength(line) < 8) {
+            if (svgLineLength(line) < 8) {
                 line.remove();
                 line = null;
                 return;
@@ -344,16 +341,16 @@ export function onLoad() {
 
             // add to history
             pushToUndoHistory(new class extends Action {
-                path: SVG.Line;
+                private path: SVG.Line;
                 constructor(path: SVG.Line) {
                     super();
                     this.path = path;
                 }
-                undo() {
+                public undo() {
                     this.path.remove();
                     lines.splice(lines.indexOf(this.path), 1);
                 }
-                redo() {
+                public redo() {
                     theSVG.add(this.path);
                     lines.push(this.path);
                 }
@@ -365,13 +362,13 @@ export function onLoad() {
     }
 
     // setup pseudo-code panel
-    let pseudoCodePanel = document.getElementById("pseudocodepanel")!;
+    const pseudoCodePanel = document.getElementById("pseudocodepanel")!;
     pseudoCode.forEach((line, index) => {
-        let p = document.createElement("pre");
+        const p = document.createElement("pre");
         p.textContent = line.code;
         p.classList.add("mb-0"); // remove bottom margin
         p.classList.add("pseudocode-line");
-        if(index == 0)
+        if (index === 0)
             p.classList.add("pseudocode-currentline");
         pseudoCodePanel.appendChild(p);
     });
