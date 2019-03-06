51132cc1e082cfe36ed249454c8795f6c7299a4b
ABCRic <ABCRic@users.noreply.github.com>
2019-03-06 19:29:02 +0000

    Fix linter breaking side-effect imports

--- src/geomvis.ts
+++ src/geomvis.ts
@@ -1,9 +1,9 @@
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import * as d3 from "d3";
+import * as SVG from "svg.js";
 import "svg.draggable.js";
 import "svg.draw.js";
-import * as SVG from "svg.js";
 import "svg.resize.js";
 import "svg.select.js";
 import "svg.select.js/dist/svg.select.css";
