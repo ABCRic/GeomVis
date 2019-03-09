3546792568e9efe73334842f6b2b2569918d1dc0
ABCRic <ABCRic@users.noreply.github.com>
2019-03-09 02:27:44 +0000

    Fix some more linter issues

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 import * as SVG from "svg.js";
 
-export function svgLineLength(line: SVG.Line) : number {
-    let points = line.array().toLine();
+export function svgLineLength(line: SVG.Line): number {
+    const points = line.array().toLine();
     return Math.sqrt(
         (points.x2 - points.x1) ** 2 +
         (points.y2 - points.y1) ** 2
